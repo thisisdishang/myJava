8bdd33e71dee67650aebc5e98113628ce382b9bb
thisisdishang <dishangkumarrana@gmail.com>
2023-12-29 19:18:29 +0530

    Day 1 of java learning

---
+++ CharDemo.java
@@ -0,0 +1,12 @@
+public class CharDemo {
+    public static void main(String[] args) {
+        char c1,c2;
+        c1=89;
+        c2='Y';
+        System.out.println("Character 1 = "+c1+" Character 2 = "+c2);
+        c1++;
+        System.out.println("Character 1 After Increment = "+c1);
+        c2--;
+        System.out.println("Character 2 After Decrement = "+c2);
+        }    
+}
---
+++ IntDemo.java
@@ -0,0 +1,8 @@
+public class IntDemo {
+    public static void main(String[] args) {
+        int i1,i2;
+        i1=65;
+        i2='A';     
+        System.err.println("Integer 1 = "+i1+" Integer 2 = "+i2);
+    }    
+}
